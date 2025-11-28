5c2dda733fe894887909f4b299752e5e0e6228c2
wingedseraph <r89647@gmail.com>
2025-11-28 20:55:30 +0300

    feat(swagger): install swagger as dependence

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
+import * as YAML from 'yamljs';
+import { AppModule } from './app.module';
 
 const PORT = Number(process.env.PORT) || 4000;
+const SWAGGER_FILE = `${process.cwd()}/doc/api.yaml` as const;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = YAML.load(SWAGGER_FILE);
+  SwaggerModule.setup('api', app, config);
+
   await app.listen(PORT);
 }
 bootstrap();
