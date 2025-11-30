e75075af9fde723e28f8bc3ff5813562f2a5f3e9
wingedseraph <r89647@gmail.com>
2025-11-30 18:09:47 +0300

    feat(favorites): implement favorites endpoint

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { FavoritesService } from './favorites.service';
+import { AlbumModule } from '../album/album.module';
+import { ArtistModule } from '../artist/artist.module';
+import { TrackModule } from '../track/track.module';
 import { FavoritesController } from './favorites.controller';
+import { FavoritesService } from './favorites.service';
 
 @Module({
   providers: [FavoritesService],
-  controllers: [FavoritesController]
+  controllers: [FavoritesController],
+  imports: [ArtistModule, AlbumModule, TrackModule],
 })
 export class FavoritesModule {}
