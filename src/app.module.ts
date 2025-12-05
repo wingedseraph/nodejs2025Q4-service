5e6d7b19255236032f1f535e0b3572ec4004213b
wingedseraph <r89647@gmail.com>
2025-12-05 16:18:54 +0300

    feat(database): install and init typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AlbumModule } from './album/album.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArtistModule } from './artist/artist.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { TrackModule } from './track/track.module';
+import { UserModel } from './user/user.model';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -14,6 +16,18 @@ import { UserModule } from './user/user.module';
     ArtistModule,
     AlbumModule,
     FavoritesModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT || '5432'),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [UserModel],
+        synchronize: true,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
