6072679e8e2556a6d4c9513f65ab2bb6951b0edb
wingedseraph <r89647@gmail.com>
2025-12-07 21:33:55 +0300

    feat(track): implement entity Track relations with entity Album & Artist

--- src/track/track.model.ts
+++ src/track/track.model.ts
@@ -1,4 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { AlbumModel } from '../album/album.model';
+import { ArtistModel } from '../artist/artist.model';
 
 @Entity('track')
 export class TrackModel {
@@ -6,10 +14,19 @@ export class TrackModel {
   id: string; // uuid v4
   @Column()
   name: string;
+
+  @ManyToOne(() => ArtistModel, { nullable: true, onDelete: 'SET NULL' })
+  @JoinColumn({ name: 'artistId' })
+  artist: ArtistModel | null;
   @Column({ nullable: true })
   artistId: string | null; // refers to Artist
+
+  @ManyToOne(() => AlbumModel, { nullable: true, onDelete: 'SET NULL' })
+  @JoinColumn({ name: 'albumId' })
+  album: AlbumModel | null;
   @Column({ nullable: true })
   albumId: string | null; // refers to Album
+
   @Column({ type: 'integer' })
   duration: number; // integer number
 }
